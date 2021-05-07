9d9f2df7b5320257a43e6fb107cb9b26dadf6eea
roverJ <243583762@qq.com>
2021-05-07 23:35:03 +0800

    init

---
+++ src/test/java/com/jake/mpdemo/MpdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jake.mpdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
